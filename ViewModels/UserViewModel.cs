faaa9e6f23444fd7e6e20b537f4196fa9b8fb31c
randika <randhikamadushan@gmail.com>
2025-03-24 20:36:41 +0000

    feat: Add login page with form validation

---
+++ ViewModels/UserViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CloudDoc.ViewModels
+{
+    public class UserViewModel
+    {
+        [Required(ErrorMessage="User Email Is Required")]
+        [Display(Name = "User Email")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; } = null!;
+
+
+        [Required(ErrorMessage = "Password Is Required")]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }
+    }
+}
